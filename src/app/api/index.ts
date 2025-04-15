bec5949b78dabad4913f1b8aa26a8e58d56e60ac
Mingjun Zhao <physicisthacker@outlook.com>
2025-04-15 16:15:09 +0800

    增加了api的模型调用代码

---
+++ src/app/api/index.ts
@@ -0,0 +1,1 @@
+export * as graphApi from './graph/route'
---
+++ src/lib/axios.ts
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+const api = axios.create({
+    baseURL: 'http://localhost:8000', // 根据你的后端地址调整
+})
+
+// 自动附加 Bearer token
+api.interceptors.request.use((config) => {
+    if (typeof window !== 'undefined') {
+        const token = localStorage.getItem('token')
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`
+        }
+    }
+    return config
+})
+
+export default api
+
