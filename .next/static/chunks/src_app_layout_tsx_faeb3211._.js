47c5c592fb0d1366e59b58ed53f71edfd4b1c8ff
iammm0 <wisewater5419@gmail.com>
2025-06-14 15:18:08 +0800

    调整下.next目录的位置

---
+++ .next/static/chunks/src_app_layout_tsx_faeb3211._.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/src_app_layout_tsx_faeb3211._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/src_app_layout_tsx_faeb3211._.js",
+  "chunks": [
+    "static/chunks/_0fccdce2._.js",
+    "static/chunks/src_app_globals_b805903d.css"
+  ],
+  "source": "dynamic"
+});
