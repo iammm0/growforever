c4445ba93af91276bd6942622bb8c2ec00e304af
iammm0 <wisewater5419@gmail.com>
2025-07-07 22:21:50 +0800

    调整 user 模型的结构

--- api/models/user.py
+++ api/models/user.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String, DateTime, func
 
-from api.core.postgres import Base
+from api.core.database import Base
+
 
 class User(Base):
     __tablename__ = "users"
