424905c882ff0ef60a28d9e9f137d0f4f71d5002
iammm0 <wisewater5419@gmail.com>
2025-07-11 17:10:57 +0800

    更新路由项

--- api/main.py
+++ api/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
 
-from api.routers import users, nodes, gpts, tests
+from routers import auth, seed, services, tests
 from utils.lifespan import lifespan
 
 app = FastAPI(lifespan=lifespan)
 
-app.include_router(users.router)
-app.include_router(nodes.router)
-app.include_router(gpts.router)
+app.include_router(auth.router)
+app.include_router(seed.router)
+app.include_router(services.router)
 app.include_router(tests.router)
