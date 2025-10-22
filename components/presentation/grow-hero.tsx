5b7c0083427c2bd7e4baa5895cfe944d61ced041
Mingjun Zhao <physicisthacker@outlook.com>
2025-10-22 16:30:46 +0800

    文本扩展不再显示留出按钮入口

--- components/presentation/grow-hero.tsx
+++ components/presentation/grow-hero.tsx
@@ -41,15 +41,6 @@ export default function GrowHero() {
           >
             开始播种想法
           </Button>
-          <Button
-              variant="contained"
-              color="success"
-              size="large"
-              className={styles.cta}
-              onClick={() => router.push('/expand')}
-          >
-            文本扩展
-          </Button>
         </Box>
       </Box>
     </Box>
